5c90d399ea98c656183937dc9f9bb23e4ef800ff
kev-69 <bismarkobuobi19@gmail.com>
2025-10-11 08:56:08 +0000

    Add CheckoutHeader component with basic styling

---
+++ my-app/src/components/CheckoutHeader.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const CheckoutHeader: React.FC = () => {
+  return (
+    <header className="bg-orange-500 h-16 w-full"></header>
+  );
+};
+
+export default CheckoutHeader;
